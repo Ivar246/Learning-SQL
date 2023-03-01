123941bc85fca50e78059ac28d39673d35b08a84
IVAR AHTSERHS <ravistha869@gmail.com>
2023-03-01 15:57:57 +0545

    display all record

---
+++ Shirts_db/problem_01.sql
@@ -0,0 +1,22 @@
+-- Problem: 
+--      display all record from table
+
+-- query:
+
+SELECT * FROM shirts;
+
+-- output:
+
+-- +----------+------------+-------+------------+-----------+
+-- | shirt_id | article    | color | shirt_size | last_worn |
+-- +----------+------------+-------+------------+-----------+
+-- |        1 | t-shirt    | white | S          |        10 |
+-- |        2 | t-shirt    | green | S          |       200 |
+-- |        3 | polo shirt | black | M          |        10 |
+-- |        4 | tank top   | Blue  | S          |        50 |
+-- |        5 | t-shirt    | pink  | S          |         0 |
+-- |        6 | polo shirt | red   | M          |         5 |
+-- |        7 | tank top   | white | S          |       200 |
+-- |        8 | tank top   | blue  | M          |        15 |
+-- +----------+------------+-------+------------+-----------
+
