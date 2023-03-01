2d2a4c73835b0e79b5f52fd8f9fc84d6550a1c16
IVAR AHTSERHS <ravistha869@gmail.com>
2023-03-01 15:59:22 +0545

    display specific column from a table

---
+++ Shirts_db/problem_03.sql
@@ -0,0 +1,23 @@
+-- Problem: 
+--      select all shirts but only print article and color
+
+-- query:
+
+SELECT article, color FROM shirts;
+
+
+-- Output:
+
+-- +------------+--------+
+-- | article    | color  |
+-- +------------+--------+
+-- | t-shirt    | white  |
+-- | t-shirt    | green  |
+-- | polo shirt | black  |
+-- | tank top   | Blue   |
+-- | t-shirt    | pink   |
+-- | polo shirt | red    |
+-- | tank top   | white  |
+-- | tank top   | blue   |
+-- | polo shirt | purple |
+-- +------------+--------+
