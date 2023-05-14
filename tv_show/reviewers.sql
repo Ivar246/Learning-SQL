d9b23428d1e5a34279b0a00df3e202d2e179e4b2
ivar246 <ravistha869@gmail.com>
2023-05-14 20:52:58 +0545

    reviewers table

---
+++ tv_show/reviewers.sql
@@ -0,0 +1,16 @@
+-- Query
+    SELECT * from reviewers;
+
+-- reviewers table
+
+-- +----+------------+-----------+
+-- | id | first_name | last_name |
+-- +----+------------+-----------+
+-- |  1 | Thomas     | Stoneman  |
+-- |  2 | Wyatt      | Skaggs    |
+-- |  3 | Kimbra     | Masters   |
+-- |  4 | Domingo    | Cortes    |
+-- |  5 | Colt       | Steele    |
+-- |  6 | Pinkie     | Petit     |
+-- |  7 | Marlon     | Crafford  |
+-- +----+------------+-----------+
