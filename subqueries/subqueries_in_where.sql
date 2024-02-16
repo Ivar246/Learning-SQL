2b9a68e10395f4232788604cb4812d7bf306f50d
Ravi Shrestha <ravistha869@gmail.com>
2024-02-16 09:56:29 +0545

    subquery in where clause

---
+++ subqueries/subqueries_in_where.sql
@@ -0,0 +1,26 @@
+Write a query that prints out the name and price of phones that have a price greater than the Samsung S5620 Monte.
+
+For reference, here is the phones table:
+
++-------------+--------------+-------+------------+
+| name        | manufacturer | price | units_sold |
++-------------+--------------+-------+------------+
+| N1280       | Nokia        | 199   | 1925       |
++-------------+--------------+-------+------------+
+| Iphone 4    | Apple        | 399   | 9436       |
++-------------+--------------+-------+------------+
+| Galaxy S    | Samsung      | 299   | 2359       |
++-------------+--------------+-------+------------+
+| S5620 Monte | Samsung      | 250   | 2385       |
++-------------+--------------+-------+------------+
+| N8          | Nokia        | 150   | 7543       |
++-------------+--------------+-------+------------+
+| Droid       | Motorola     | 150   | 8395       |
++-------------+--------------+-------+------------+
+| Wave S8500  | Samsung      | 175   | 9259       |
++-------------+--------------+-------+------------+
+
+Solution->
+    select name, price from phones where price > (
+    select price from phones where name='S5620 Monte'
+    );
